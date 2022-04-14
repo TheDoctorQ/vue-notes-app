709f5cee0890bf4d1dd7c43cc6212e055e510db8
TheDoctorQ <ilias.schwarz42@gmail.com>
2022-04-14 14:04:08 -0600

    init

---
+++ src/components/NoteContainer.vue
@@ -0,0 +1,24 @@
+<script>
+import NoteSelectors from './NoteSelectors.vue'
+import NoteEditor from './NoteEditor.vue'
+export default {
+  components: {
+    NoteSelectors,
+    NoteEditor
+  },
+  props: ["notes", "selectedNoteId"],
+  methods: {
+    selectNote: function (note) {
+      this.$emit("selectNote", note)
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="note-container">
+    <NoteSelectors v-bind:notes="notes" v-bind:selectedNoteId="selectedNoteId" v-on:selectNote="selectNote" />
+    <NoteEditor />
+
+  </div>
+</template>
---
+++ src/components/NoteSelector.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  props: ["note", "selectedNoteId"],
+  methods: {
+    selectNote: function (note) {
+      this.$emit("selectNote", note);
+      console.log('in note selector')
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="note-selector" v-bind:class="{ active: selectedNoteId === note.id }" v-on:click="selectNote(note)">
+    <p class="note-selector-title">{{ note.body }}</p>
+    <p class="note-selector-title">({{ selectedNoteId }}) {{ note.id }}</p>
+    <p class="note-selector-timestamp">Timestamp here {{ note.timestamp }}</p>
+  </div>
+</template>
